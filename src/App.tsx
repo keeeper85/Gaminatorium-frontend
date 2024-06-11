f5fa4c134605e693f63ae226159011e6cf426656
Saracier <105667230+Saracier@users.noreply.github.com>
2024-06-11 17:28:11 +0200

    feat: add reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 import Layout from "./layout/Layout"
 
-function App() {
+function App()  {
   return (
     <Layout>
       <h1 className="text-red">TEST</h1>
