456b797d24a9bd1ccda505379e0fdf8d3f28c560
Ombre <krys1014@wp.pl>
2024-06-19 19:59:35 +0200

    fix: change font-size, add box-shadow to image and remove unnecessery div

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,17 +1,14 @@
-
 export default function Hero() {
   return (
     <div className="flex flex-col md:flex-row gap-12 mt-12 items-center">
       <div className="flex flex-col text-center gap-4">
-        <h1 className="flex flex-col font-irish xl:text-5xl text-4xl font-bold">
+        <h1 className="flex flex-col font-irish xl:text-6xl text-4xl font-bold">
           Welcome to <strong className="text-red">Gaminatorium</strong>
         </h1>
         <span className="text-xs">Experiment. Learn. Play.</span>
       </div>
 
-      <div>
-        <img src="/assets/images/Hero_image.jpg" className="rounded-3xl w-64 h-64" alt="Castle with Moon" />
-      </div>
+      <img src="/assets/images/Hero_image.jpg" className="rounded-3xl xl:w-80 xl:h-80 w-64 h-64 shadow-[6px_4px_20px_-3px_rgba(0,0,0,1)]" alt="Castle with Moon" />
     </div>
   )
 }
