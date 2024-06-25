be1440aa43a1d543d001d11e0822371d0e0614c0
Saracier <105667230+Saracier@users.noreply.github.com>
2024-06-25 16:07:55 +0200

    fix: fix font hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
         <h1 className="flex flex-col font-irish xl:text-6xl text-4xl font-bold">
           Welcome to <strong className="text-red">Gaminatorium</strong>
         </h1>
-        <span className="text-xs">Experiment. Learn. Play.</span>
+        <span className="text-base font-irish">Experiment. Learn. Play.</span>
       </div>
 
       <img src="/assets/images/Hero_image.jpg" className="rounded-3xl xl:w-80 xl:h-80 w-64 h-64 shadow-[6px_4px_20px_-3px_rgba(0,0,0,1)]" alt="Castle with Moon" />
