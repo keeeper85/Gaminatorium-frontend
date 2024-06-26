4d86f60f6a2a004edb096ae9f5907ebb26ad63f9
Saracier <105667230+Saracier@users.noreply.github.com>
2024-06-26 16:44:44 +0200

    feat: add logic to errors

--- src/core/Login/LoginForm.tsx
+++ src/core/Login/LoginForm.tsx
@@ -1,17 +1,74 @@
 import {useState} from "react";
 import {Box, Button, InputBase, Paper} from "@mui/material";
+import { z } from 'zod';
+
+const loginSchema = z.object({
+    email: z.string().email({ message: "Invalid email format" }).nonempty({ message: "Email is required" }),
+    password: z.string().min(1, { message: "Password is required" })
+});
 
 function LoginForm() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({ email: '', password: '' });
 
-    const handleSubmit = (event: any) => {
+    const handleEmailCheck = (event:any) => {
         event.preventDefault();
-        // Tutaj można dodać logikę logowania
-        console.log('Username:', username, 'Password:', password);
+        const result = loginSchema.safeParse({
+            email: username,
+            password: password
+        });
+        let emailError = '';
+        result.error?.issues.forEach(issue => {
+            if(issue.path[0] === 'email') {
+                emailError = issue.message;
+            }
+
+        })
+            setErrors(errors => {
+                return {
+                    ...errors,
+                    email: emailError
+                }
+            })
+    }
+
+    const handlePasswordCheck = (event:any) => {
+        event.preventDefault();
+        const result = loginSchema.safeParse({
+            email: username,
+            password: password
+        });
+        let passwordError = '';
+        result.error?.issues.forEach(issue => {
+            if(issue.path[0] === 'password') {
+                passwordError = issue.message;
+            }
+        })
+        setErrors( {
+
+            ...errors,
+            password: passwordError
+
+        })
+    }
+    const handleSubmit = async (event : any) => {
+        console.log(event)
+        event.preventDefault();
+
+        const result = loginSchema.safeParse({
+            email: username,
+            password: password
+        });
+        console.log(result);
+        if(result.success) {
+                setErrors({ email: '', password: '' });
+                console.log("Form is valid: ", result.data);
+            return
+        }
     };
 
-    return (
+    return <>
         <Box
             component="form"
             sx={{
@@ -34,10 +91,15 @@ function LoginForm() {
                     sx={{ ml: 1, flex: 1 }}
                     inputProps={{ 'aria-label': 'enter your username' }}
                     value={username}
-                    onChange={e => setUsername(e.target.value)}
+                    onChange={e =>
+                        setUsername(e.target.value)
+
+
+                    }
+                    onBlur={e => handleEmailCheck(e) }
                 />
             </Paper>
-            <span className="m-2 self-start text-xs font-inknut text-red">email is empty!</span>
+            <span className="m-2 self-start text-xs font-inknut text-red">{errors.email || '\u00A0'}</span>
             <span className="m-2 self-start text-base font-inknut">password</span>
             <Paper
                 component="form"
@@ -49,11 +111,14 @@ function LoginForm() {
                     type="password"
                     inputProps={{ 'aria-label': 'enter your password' }}
                     value={password}
-                    onChange={e => setPassword(e.target.value)}
+                    onChange={e => {
+                        setPassword(e.target.value)
+                    }}
+                    onBlur={e => handlePasswordCheck(e) }
                 />
             </Paper>
-            <div className="m-2 text-xs font-inknut flex space-x-6">
-            <span className=" text-red">password is empty!</span>
+            <div className="m-2 text-xs font-inknut flex  justify-between w-full">
+            <span className=" text-red">{errors.password || '\u00A0'}</span>
             <span >Forgot your password?</span>
 
             </div>
@@ -63,7 +128,7 @@ function LoginForm() {
                 Login
             </Button>
         </Box>
-    );
+    </>
 }
 
 export default LoginForm
