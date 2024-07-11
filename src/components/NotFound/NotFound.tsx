9a2312a68c8b386830f33545134533893080d1c1
Karol <karol.lichon@zsz.bobowa.pl>
2024-07-11 16:42:32 +0200

    create 404 page and added route

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,30 @@
+export default function NotFound() {
+	return (
+		<div className='flex flex-col items-center justify-center h-screen p-8 text-center font-irish lg:items-end xl:pr-40'>
+			<div className='relative lg:static'>
+
+				<img
+					src='/assets/images/404.png'
+					className='w-[221px] aspect-square mb-4 lg:absolute lg:left-[26px] lg:w-[516px] lg:bottom-[26px] lg:z-[-1] xl:w-[40%] xl:max-w-[1000px]'
+					alt='Not Found Image'
+				/>
+				<h1 className='text-9xl mb-4 absolute top-[-77px] z-[-1] lg:static lg:text-[12rem] lg:mb-20 xl:text-[24rem]'>
+					404
+				</h1>
+
+			</div>
+
+			<p className='text-base mb-12 max-w-[330px] lg:text-2xl lg:max-w-[500px] lg:mb-[10%] xl:text-4xl xl:max-w-[750px]'>
+				We are very sorry. The connection has been severed. Please return to the homepage or contact us!
+			</p>
+
+			<button className='text-base py-4 px-8 my-2 bg-[#70211C] text-white border-solid border-2 border-[#70211C] rounded-lg w-full max-w-xs transition duration-300 hover:bg-transparent xl:text-3xl xl:py-8 xl:max-w-[26rem]'>
+				Back to Home
+			</button>
+			<button className='text-base py-4 px-8 my-2 bg-[#70211C] text-white border-solid border-2 border-[#70211C] rounded-lg w-full max-w-xs transition duration-300 hover:bg-transparent xl:text-3xl xl:py-8 xl:max-w-[26rem]'>
+				Contact Us
+			</button>
+
+		</div>
+	)
+}
