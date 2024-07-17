e30195f2b7961533d0ae20ddd71f18ed136a6cff
Karol <karol.lichon@zsz.bobowa.pl>
2024-07-17 11:49:24 +0200

    reapir register page

--- src/core/Register/Register.tsx
+++ src/core/Register/Register.tsx
@@ -1,24 +1,11 @@
-import Navbar from '../../components/Navbar/Navbar'
-import { AuthProvider } from '../../contexts/Authcontext'
 import { RegisterForm } from './RegisterForm'
 
 export default function Register() {
 	return (
-		<AuthProvider>
-			<header className='flex justify-center'>
-				<Navbar />
-			</header>
-		
-            <main className='flex justify-center items-center min-h-screen p-6 lg:w-full'>
+		<section className='w-ful p-8 rounded-2xl bg-black w-full max-w-[435px] shadow-dark mt-28 lg:p-16'>
+			<h1 className='font-irish text-center text-4xl mb-4 lg:text-6xl lg:mb-14'>Register</h1>
 
-                <section className='w-ful p-16 rounded-2xl bg-black w-full max-w-[435px] shadow-dark mt-28 '>
-                    <h1 className='font-irish text-center text-4xl mb-4 lg:text-6xl lg:mb-14'>Register</h1>
-
-                    <RegisterForm />
-                    {/*{children}*/}
-                </section>
-
-            </main>
-		</AuthProvider>
+			<RegisterForm />
+		</section>
 	)
 }
