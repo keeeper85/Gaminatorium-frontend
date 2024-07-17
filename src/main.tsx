68242afd54695a4a1fea3a346495395a3783407e
Xmausix <barbara.szymanska469@gmail.com>
2024-07-17 13:45:04 +0200

    Update main.tsx Route component

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material';
-import darkTheme from "./themes/darkTheme.ts";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import Login from "./core/Login/Login.tsx";
+import { ThemeProvider } from '@mui/material';
+import darkTheme from "./themes/darkTheme.ts";
+import Footer from './components/Footer/Footer.tsx';
+import Carousel from './components/Carousel/Carousel.tsx';
+import Hero from './components/Hero/Hero.tsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <ThemeProvider theme={darkTheme}>
           <Router>
-              <App />
+            <Routes>
+                <Route path='/hero' Component={Hero} />
+                <Route path='/Carousel' Component={Carousel} />
+                <Route path='/Footer' Component={Footer} />
+                <Route path="/" Component={App} />
+                <Route path="/login" Component={Login} />
+            </Routes>
           </Router>
       </ThemeProvider>
   </React.StrictMode>,
