faeb07adae079ef1c4388b068f7c3a7c04130f4c
Xmausix <barbara.szymanska469@gmail.com>
2024-07-17 13:37:39 +0200

    Update Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Layout from "./core/layout/Layout"
 import Hero from "./components/Hero/Hero"
 
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+
 function App()  {
   return (
     <Layout>
+      <Navbar/>
       <Hero />
+      
+      <Footer />
     </Layout>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import Login from "./core/Login/Login.tsx";
+import { ThemeProvider } from '@mui/material';
+import darkTheme from "./themes/darkTheme.ts";
+import Footer from './components/Footer/Footer.tsx';
+import Carousel from './components/Carousel/Carousel.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" Component={App} />
-      </Routes>
-    </Router>
+      <ThemeProvider theme={darkTheme}>
+          <Router>
+            <Routes>
+                
+                <Route path='/Carousel' Component={Carousel} />
+                <Route path='/Footer' Component={Footer} />
+                <Route path="/" Component={App} />
+                <Route path="/login" Component={Login} />
+            </Routes>
+          </Router>
+      </ThemeProvider>
   </React.StrictMode>,
 )
