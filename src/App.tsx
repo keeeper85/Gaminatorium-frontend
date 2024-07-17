83bff7a8bb7b98023b6cb85225bd67fa5c21bbb0
Karol <karol.lichon@zsz.bobowa.pl>
2024-07-17 13:31:41 +0200

    created structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Layout from './core/layout/Layout.tsx'
-import Hero from './components/Hero/Hero'
 import { Route, Routes } from 'react-router-dom'
 import Login from './core/Login/Login.tsx'
 import Page404 from './core/Page404/Page404.tsx'
@@ -7,12 +6,13 @@ import About from './core/About/About.tsx'
 import Register from './core/Register/Register.tsx'
 import ChangePassword from './core/ChangePassword/ChangePassword.tsx'
 import ForgotPassword from './core/ForgotPassword/ForgotPassword.tsx'
+import Home from './core/Home/Home.tsx'
 
 export default function App() {
 	return (
 		<Routes>
 			<Route element={<Layout />}>
-				<Route path='/' element={<Hero />} />
+				<Route path='/' element={<Home />} />
 				<Route path='/login' element={<Login />} />
 				<Route path='/register' element={<Register />} />
 				<Route path='/change-password' element={<ChangePassword />} />
