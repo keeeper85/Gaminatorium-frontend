f4f7ce16eb06cb67c69a57392f96143740046f9b
Xmausix <barbara.szymanska469@gmail.com>
2024-07-21 11:50:35 +0200

    Update line position

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,5 +1,5 @@
 export function Line () {
     return (
-        <div className='h-[1px] w-[80%] bg-[#DDD] mx-auto'></div>
+        <div className='h-[1px] w-[80%] bg-[#DDD] mx-auto ml-40'></div>
     )
 }
