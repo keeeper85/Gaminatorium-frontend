431e58c3645d5f900514167d6b775c705ca6cf68
Xmausix <barbara.szymanska469@gmail.com>
2024-07-21 11:54:23 +0200

    Update Line.tsx

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,5 +1,5 @@
 export function Line () {
     return (
-        <div className='h-[1px] w-[80%] bg-[#DDD] mx-auto ml-40'></div>
+        <div className='h-[1px] w-[65%] bg-[#DDD] mx-auto ml-47'></div>
     )
 }
